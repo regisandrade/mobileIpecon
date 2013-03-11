a28b4cb12256a55cc012785a4837d7b27ae6fb69
Regis <regisandrade@gmail.com>
2013-03-11 13:44:45 -0300

    novos arquivos - commit

---
+++ cursos/cursos.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+        <title>IPECON Pós-Graduação</title>
+
+        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.css" />
+        <link rel="stylesheet" href="../css/my.css" />
+        <script src="http://code.jquery.com/jquery-1.7.2.min.js">
+        </script>
+        <script src="http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js">
+        </script>
+        <script src="../js/my.js">
+        </script>
+        <!-- User-generated css -->
+        <style>
+        </style>
+        <!-- User-generated js -->
+        <script>
+            try {
+
+    $(function() {
+
+    });
+
+  } catch (error) {
+    console.error("Your javascript has an error: " + error);
+  }
+        </script>
+    </head>
+    <body>
+        <!-- Home -->
+        <div data-role="page" id="page1">
+            <div data-theme="b" data-role="header">
+                <h1 class="header">
+                    IPECON
+                </h1>
+            </div>
+            <div data-role="content">
+                <h2>
+                    Cursos
+                </h2>
+                <a data-role="button" data-inline="true" data-rel="back" data-theme="e" href="#page1">
+                    Voltar
+                </a>
+            </div>
+            <div data-theme="b" data-role="footer" data-position="fixed">
+                <h1 class="footer">
+                    Av. T-4, nº 1.478, Ed. Absolut Business Style, sala A-132 (13º andar)<br>
+                    Setor Bueno, Goiânia-GO
+                </h1>
+            </div>
+        </div>
+    </body>
+</html>
