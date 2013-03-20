2219bf8879de839061b9376c00d61997641532cd
Regis <regisandrade@gmail.com>
2013-03-20 14:09:14 -0300

    criando dialog para os alerts

--- contato/enviarEmail.php
+++ contato/enviarEmail.php
@@ -1,21 +1,27 @@
 <?php
-//$to      = "IPECON - Ensino e consultoria <ipecon@ipecon.com.br>";
-$to      = "Regis Andrade <regisandrade@gmail.com>";
-$subject = $_REQUEST['txtAssunto'];
-$message = $_REQUEST['txtMensagem'];
-
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-$headers .= 'From: '.$_REQUEST['txtEmail'].' <'.$_REQUEST['txtEmail'].'>' . "\r\n";
-$headers .= 'Reply-To: '.$_REQUEST['txtEmail'].' <'.$_REQUEST['txtEmail'].'>' . "\r\n";
-
-if(!mail($to, $subject, $message, $headers)){
-	$var['msg'] = "Erro no envio do e-mail.";
+// Validar os campo
+if(!$_REQUEST['txtEmail'] || !$_REQUEST['txtAssunto'] || !$_REQUEST['txtMensagem'] ){
+	$var['msg']     = "Por favor, preencher todos os campos para envio do e-mail!";
 	$var['sucesso'] = false;
-	mail("regisandrade@gmail.com", "[Não responder] Erro no envio de e-mail", "Erro ao tentar enviar e-mail pelo site mobile do IPECON.");
 }else{
-	$var['msg'] = "E-mail enviado com sucesso.";
-	$var['sucesso'] = true;
+	//$to      = "IPECON - Ensino e consultoria <ipecon@ipecon.com.br>";
+	$to      = "Regis Andrade <regisandrade@gmail.com>";
+	$subject = $_REQUEST['txtAssunto'];
+	$message = $_REQUEST['txtMensagem'];
+
+	$headers  = 'MIME-Version: 1.0' . "\r\n";
+	$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
+	$headers .= 'From: '.$_REQUEST['txtEmail'].' <'.$_REQUEST['txtEmail'].'>' . "\r\n";
+	$headers .= 'Reply-To: '.$_REQUEST['txtEmail'].' <'.$_REQUEST['txtEmail'].'>' . "\r\n";
+
+	if(!mail($to, $subject, $message, $headers)){
+		$var['msg']     = "Erro no envio do e-mail.";
+		$var['sucesso'] = false;
+		mail("regisandrade@gmail.com", "[Não responder] Erro no envio de e-mail", "Erro ao tentar enviar e-mail pelo site mobile do IPECON.");
+	}else{
+		$var['msg']     = "E-mail enviado com sucesso.";
+		$var['sucesso'] = true;
+	}	
 }
 echo json_encode($var);
 ?>
--- js/my.js
+++ js/my.js
@@ -4,12 +4,12 @@
 $(document).ajaxStop($.unblockUI); 
 
 $(document).ready(function(){
-    $.blockUI({ message: '<h2><img src="../imagens/ajax-loader.gif" /><br>Carregando...</h2>' });
-
 	// Enviar e-mail
 	$('#btnEnviarEmail').click(function() {
 		enviarEmail();
 	});
+
+	$.blockUI({ message: '<h2><img src="../imagens/ajax-loader.gif" /><br>Carregando...</h2>' });
 });
 
 /**
@@ -33,22 +33,48 @@ function _GET(name)
 	return null;
 }
 
+/* Modelo de Dialog para mobile */
+function alertaMobile(seletor,msg){
+	$("#dialogModal").remove();
+	var $divDialog = "<div role=\"dialog\" class=\"ui-dialog-contain ui-corner-all ui-overlay-shadow\" id=\"dialogModal\">"
+					+"	<div data-role=\"header\" data-theme=\"d\" class=\"ui-corner-top ui-header ui-bar-d\" role=\"banner\">"
+					+"		<a href=\"#\" data-icon=\"delete\" data-iconpos=\"notext\" class=\"ui-btn-left ui-btn ui-btn-up-d ui-shadow ui-btn-corner-all ui-btn-icon-notext\" data-corners=\"true\" data-shadow=\"true\" data-iconshadow=\"true\" data-wrapperels=\"span\" data-theme=\"d\" title=\"Fechar\">"
+					+"			<span class=\"ui-btn-inner ui-btn-corner-all\">"
+					+"				<span class=\"ui-btn-text\">Fechar</span>"
+					+"				<span class=\"ui-icon ui-icon-delete ui-icon-shadow\">&nbsp;</span>"
+					+"			</span>"
+					+"		</a>"
+					+"		<h1 class=\"ui-title\" role=\"heading\" aria-level=\"1\">Atenção</h1>"
+					+"	</div>"
+					+"	<div data-role=\"content\" data-theme=\"c\" class=\"ui-corner-bottom ui-content ui-body-c\" role=\"main\">"
+					+msg+
+					+"		<a href=\"contato.html\" data-role=\"button\" data-rel=\"back\" data-theme=\"c\" data-corners=\"true\" data-shadow=\"true\" data-iconshadow=\"true\" data-wrapperels=\"span\" class=\"ui-btn ui-shadow ui-btn-corner-all ui-btn-up-c\">"
+					+"			<span class=\"ui-btn-inner ui-btn-corner-all\">"
+					+"				<span class=\"ui-btn-text\">Cancel</span>"
+					+"			</span>"
+					+"		</a>"
+					+"	</div>"
+					+"</div>";
+	$(seletor).appendTo($divDialog);
+	
+}
+
 function enviarEmail(){
 	var dados = $('#formContato').serialize();
-
 	$.ajax({
 		type     : 'POST',
 		dataType : 'JSON',
-		url      : 'http://m.ipecon.com.br/enviarEmail.php',
+		url      : 'http://m.ipecon.com.br/contato/enviarEmail.php',
 		data     : dados,
 		success  : function(retorno){
 			if(retorno.sucesso == 'true') {
-				$('txtEmail').val() = "";
-				$('txtAssunto').val() = "";
-				$('txtMensagem').val() = "";
-				history.back(-1);
+				$('txtEmail').val('');
+				$('txtAssunto').val('');
+				$('txtMensagem').val('');
+				//history.back(-1);
+				alertaMobile('#pageContato',retorno.msg);
 			}else{
-				alert(retorno.msg);
+				alertaMobile('#pageContato',retorno.msg);
 			}
 		}
 	});
